f8988666f8aac4573b1c4da198e402fd41fb0881
yesheytenzin <yesheytenzin09@gmail.com>
2026-07-24 13:41:58 +0600

    test: rspec for session and registrations

--- spec/requests/auth/registrations_spec.rb
+++ spec/requests/auth/registrations_spec.rb
@@ -6,8 +6,41 @@ RSpec.describe "Api::V1:Auth:Registrations", type: :request do
   let!(:user) { create(:user) }
 
   describe "POST /api/v1/auth/sign_up" do
-    subject(:sign_up_request) do
+    context "with valid credentials" do
+      subject(:sign_up_request) do
+        post user_registration_path, params: { user: valid_user_params }
+        response
+      end
 
+      let(:valid_user_params) { attributes_for(:user) }
+
+      it "test registration" do
+        sign_up_request
+        expect(response).to have_http_status(:created)
+        expect(json[:user][:email]).to eq valid_user_params[:email]
+      end
+    end
+
+    context "with invalid credentials" do
+      subject(:sign_up_request) do
+        post user_registration_path, params: { user: invalid_user_params }
+        response
+      end
+
+      let(:invalid_user_params) {
+        {
+          user: {
+            email: Faker::Internet.email,
+            password: Faker::Internet.password(min_length: 8),
+            password_confirmation: Faker::Internet.password(min_length: 8)
+          }
+        }
+      }
+
+      it "test registration fail" do
+        sign_up_request
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
     end
   end
 end
---
+++ spec/requests/auth/sessions_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe "Api::V1::Auth::Sessions", type: :request do
+  let(:raw_password) { Faker::Internet.password(min_length: 8) }
+  let!(:valid_user) { create(:user, password: raw_password, password_confirmation: raw_password) }
+
+  describe "POST /api/v1/auth/sign_in" do
+    context "with valid credentials" do
+      subject(:sign_in_request) do
+        post user_session_path, params: { user: valid_user_params }
+        response
+      end
+
+      let(:valid_user_params) {{ email: valid_user.email, password: valid_user.password }}
+      it " Login tests " do
+        sign_in_request
+        expect(response).to have_http_status(:ok)
+        expect(json.with_indifferent_access["user"]["email"]).to eq(valid_user_params[:email])
+        expect(response.headers["Set-Cookie"]).to be_present
+      end
+    end
+
+    context "with invalid credentials" do
+      let(:wrong_password) { "password123" }
+      subject(:sign_in_request) do
+        post user_session_path, params: { user: invalid_user_params }
+        response
+      end
+
+      let(:invalid_user_params) {{ email: Faker::Internet.email, password: wrong_password }}
+      it "login fail test" do
+        sign_in_request
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  describe "DELETE /api/v1/auth/sign_out" do
+    let(:raw_password) { Faker::Internet.password(min_length: 8) }
+    let!(:valid_user) { create(:user, password: raw_password, password_confirmation: raw_password) }
+    let(:valid_user_params) {{ email: valid_user.email, password: valid_user.password }}
+
+    subject(:sign_out_request) do
+      post user_session_path, params: { user: valid_user_params }
+      delete destroy_user_session_path
+      response
+    end
+
+    it "logs out" do
+      sign_out_request
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
+
+
+
