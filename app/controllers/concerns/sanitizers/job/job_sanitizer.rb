adf2f4c1247d9f60d0eb13c30ee19a554b72e75d
yesheytenzin <yesheytenzin09@mail.com>
2026-07-29 11:11:51 +0600

    controller: job

---
+++ app/controllers/concerns/sanitizers/job/job_sanitizer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Sanitizers
+  module Job
+    module JobSanitizer
+      private
+
+      def job_params
+        params.expect(job: [ :title, :description, :min_salary, :max_salary, :status ])
+      end
+    end
+  end
+end
