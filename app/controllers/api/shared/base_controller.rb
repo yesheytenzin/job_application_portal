18d913f064800c6f8b22867b0fe66d653a4b6fa8
yesheytenzin <yesheytenzin09@mail.com>
2026-08-02 22:00:22 +0600

    refactor

--- app/controllers/api/shared/base_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Api
-  module Shared
-    class BaseController < ApplicationController
-      before_action :authenticate_user!
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,38 @@
-# config/routes.rb
-
 Rails.application.routes.draw do
   devise_for :users,
-    path: 'api/guard',
-    path_names: {
-      sign_in: 'sign_in',
-      sign_out: 'sign_out',
-      registration: 'sign_up'
-    },
-    controllers: {
-      sessions: 'api/guard/sessions',
-      registrations: 'api/guard/registrations'
-    }
+             path: 'api/guard',
+             path_names: {
+               sign_in: 'sign_in',
+               sign_out: 'sign_out',
+               registration: 'sign_up'
+             },
+             controllers: {
+               sessions: 'api/guard/sessions',
+               registrations: 'api/guard/registrations'
+             }
 
   namespace :api do
-    namespace :shared do
-      resource :profile,
-        only: [ :show, :update ],
-        controller: '/api/shared/profiles'
-    end
-
-    namespace :job do
-      namespace :v1 do
-        resources :jobs, only: [ :index, :show ], controller: '/api/job/v1/jobs'
-      end
-    end
-
     namespace :admin do
       namespace :v1 do
-        resources :jobs, only: [ :create, :update, :destroy ], controller: '/api/admin/v1/jobs'
-        resources :job_applications, only: %i[ index show update destroy ], controller: '/api/admin/v1/job_applications'
+        resources :jobs, only: %i[create update destroy], controller: '/api/admin/v1/jobs'
+
+        resources :job_applications, only: %i[index show update destroy], controller: '/api/admin/v1/job_applications'
       end
     end
 
     namespace :user do
       namespace :v1 do
-        resources :job_applications, only: %i[ index show create ], controller: '/api/user/v1/job_applications'
+        resources :job_applications,
+                  only: %i[index show create],
+                  controller: '/api/user/v1/job_applications'
+
+        resource :profile,
+                 only: %i[show update],
+                 controller: '/api/user/v1/profiles'
+
+        resources :jobs,
+                  only: %i[index show],
+                  controller: '/api/user/v1/jobs'
       end
     end
   end
