111a733537c85a4db432c110429a9bca85be39cb
yesheytenzin <yesheytenzin09@mail.com>
2026-08-02 22:10:03 +0600

    attachment serializer setup

--- app/serializers/job_application_serializer.rb
+++ app/serializers/job_application_serializer.rb
@@ -1,29 +1,16 @@
 # frozen_string_literal: true
 
 class JobApplicationSerializer < Blueprinter::Base
+  extend AttachmentSerializer
   identifier :id
   fields :status, :user_id, :job_id
 
   fields :resume do |job_application|
-    attached_payload(job_application.resume)
+    attachment_payload(job_application.resume)
   end
 
   fields :cover_letter do |job_application|
-    attached_payload(job_application.cover_letter)
-  end
-
-  private
-
-  def attached_payload(attachment)
-    return unless attachment.attached?
-    blob = attachment.blob
-
-    {
-      id: blob.id,
-      filename: blob.filename.to_s,
-      content_type: blob.content_type,
-      url: Rails.application.routes.url_helpers.rails_blob_url(blob)
-    }
+    attachment_payload(job_application.cover_letter)
   end
 end
 
--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,21 +1,11 @@
 # frozen_string_literal: true
 
 class JobSerializer < Blueprinter::Base
+  include AttachmentSerializer
   identifier :id
   fields :title, :description, :min_salary, :max_salary, :status
 
   fields :attachments do |job|
-    job.attachments.map do |attachment|
-      {
-        id: attachment.id,
-        filename: attachment.filename.to_s,
-        content_type: attachment.blob.content_type,
-        byte_size: attachment.blob.byte_size,
-        url: Rails.application.routes.url_helpers.rails_blob_url(
-          attachment,
-          host: 'localhost:3000'
-        )
-      }
-    end
+    attachments_payload(job.attachments)
   end
 end
